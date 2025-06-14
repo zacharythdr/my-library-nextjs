780ca667789de36a2612db03dd1efdb00c18587f
ZacharyTheodoreCodes <zacharytheodore.zac@gmail.com>
2025-06-14 17:57:23 +0700

    read data

--- src/app/library/page.js
+++ src/app/library/page.js
@@ -1,8 +1,35 @@
 import React from "react";
 import { getUsername } from "../utils/getUsername";
+import Image from "next/image";
 
 export default async function Page() {
   const username = await getUsername();
+  const res = await fetch(`https://v1.appbackend.io/v1/rows/LSHWjLWo4iOE`);
+  const { data } = await res.json();
 
-  return <p>Hello {username}!</p>;
+  return (
+    <div>
+      <h1>My Library</h1>
+      <h2>Hello {username}</h2>
+
+      <div>
+        {data.map((book) => {
+          return (
+            <div key={book._id}>
+              <Image
+                src={book.image}
+                alt={book.title}
+                width={200}
+                height={200}
+                unoptimized
+              ></Image>
+              <h3>{book.title}</h3>
+              <p>{book.review}</p>
+              <p>{book.rating}/5</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
