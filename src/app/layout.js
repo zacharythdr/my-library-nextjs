ecbe1dbd5800c2f462a01b5a702aa07d2db270c2
ZacharyTheodoreCodes <zacharytheodore.zac@gmail.com>
2025-06-14 16:34:35 +0700

    init commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import { HeroUIProvider } from "@heroui/react";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -22,7 +24,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <HeroUIProvider>{children}</HeroUIProvider>
       </body>
     </html>
   );
